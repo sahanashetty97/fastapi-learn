4d66e2d12b0801b56c61ecb4511907037823b8bf
Shivashankar <SSHIVAS1@volvocars.com>
2024-04-15 14:20:27 +0200

    added .gitignore

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+from fastapi.testclient import TestClient
+from fastapi-learn.main import app
+
+client = TestClient(app)
+
+def test_read_main():
+    response = client.get("/")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Hello, world!"}
